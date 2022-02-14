f0d19c6ebff49a18a64cc8442ba9f76260611aec
yk7335 <60045080+yk7335@users.noreply.github.com>
2022-02-14 14:09:36 -0600

    added folders for unit test and rspec on gem file

---
+++ spec/feature/Integration_spec.rb
@@ -0,0 +1,20 @@
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+# Example of Integration test
+
+# RSpec.describe 'Creating a book', type: :feature do
+#    scenario 'valid inputs' do
+#      visit new_book_path
+#      fill_in 'Title', with: 'harry potter'
+#      click_on 'Create Book'
+#      visit books_path
+#      expect(page).to have_content('harry potter')
+#    end
+#  end
+
+# COMMAND TO RUN : rspec spec/feature/integration_spec.rb
+
+# have to go to app/model/user.rb to validate the feilds
+
+# This doc is the TDD on canvas : https://docs.google.com/document/d/1mS5kiGjjqbpg_ckmAx3RkvkS8a--MjqN/edit
