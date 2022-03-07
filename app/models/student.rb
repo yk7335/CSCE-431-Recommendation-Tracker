6cfa677fc5c32159f79a9755f751a29c1d9fbe77
Mihiran Pandey <70815387+MihiranP@users.noreply.github.com>
2022-03-07 15:13:57 -0600

    Update student.rb
    Need to comment out so csv upload can be tested on heroku.

--- app/models/student.rb
+++ app/models/student.rb
@@ -21,16 +21,17 @@ class Student < ApplicationRecord
     end
   end
 
-  validates :email, presence: true
-  validates :firstname, presence: true
-  validates :lastname, presence: true
-  validates :notes, presence: true
-  validates :uin, presence: true
-  validates :major, presence: true
-  validates :finalgrade, presence: true
-  validates :updatedgrade, presence: true
-  validates :classname, presence: true
-  validates :recletter, presence: true
-  validates :semester, presence: true
-  validates :year, presence: true
+#   --- remove for heroku deployment testing ---
+#   validates :email, presence: true
+#   validates :firstname, presence: true
+#   validates :lastname, presence: true
+#   validates :notes, presence: true
+#   validates :uin, presence: true
+#   validates :major, presence: true
+#   validates :finalgrade, presence: true
+#   validates :updatedgrade, presence: true
+#   validates :classname, presence: true
+#   validates :recletter, presence: true
+#   validates :semester, presence: true
+#   validates :year, presence: true
 end
